90df97c16fa8e171de4081d985c18c6532d83cf5
Destiny Saturday <yiradesat@gmail.com>
2023-03-10 15:52:11 +0100

    closed

--- classes.dart
+++ classes.dart
@@ -1,21 +1,27 @@
 void main() {
   //Create person class
-  Person p1 = Person('John', "male", 21);
-  //p1.showData();
-  print(p1.name);
+  Person p1 = Person();
+  p1.showData();
 
-  //Create second person
+  //Add data
+  p1.name = "John";
+  p1.age = 20;
+  p1.sex = "male";
+
+  p1.showData();
+  /*Create second person
   Person p2 = Person('Mary', "female", 20);
   p2.showData();
   print(p2.name);
+  */
 }
 
 class Person {
   String? name, sex;
   int? age;
 
-  //Constructor
-  Person(String name, sex, int age) {
+  //Method
+  void addData(String name, sex, int age) {
     this.name = name;
     this.sex = sex;
     this.age = age;
