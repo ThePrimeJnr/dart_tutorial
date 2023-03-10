9a55230ac864cb7d5017444258534303c3a39fd0
Destiny Saturday <yiradesat@gmail.com>
2023-03-10 11:38:19 +0100

    Logic completed

---
+++ logic.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Logic -if, if/else, else
+  var num = 51;
+  // > < >= <= == !=
+  if (num == 5) {
+    print("The number is 5.");
+  } else if (num == 3) {
+    print("The number is 3.");
+  } else {
+    print("The number is not 3 or 5, it is $num.");
+  }
+}
