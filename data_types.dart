ccb41d79f79697f331d01db15a9afa3c07466571
Destiny Saturday <yiradesat@gmail.com>
2023-03-10 08:55:56 +0100

    dat types numbers

--- data_types.dart
+++ data_types.dart
@@ -4,5 +4,11 @@ void main() {
   //Strings
   String firstName = "Destiny";
   String lastName = "Saturday";
-  print("My first name is $firstName $lastName");
+  print("My first name is $firstName $lastName,");
+
+  //Numbers
+
+  //Integers
+  int age = 19;
+  print(" I am $age years old.");
 }
