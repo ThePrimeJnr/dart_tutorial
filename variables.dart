6c281eb470d165edf45a64ecd45dd16d07c02f32
Destiny Saturday <yiradesat@gmail.com>
2023-03-10 08:41:27 +0100

    dart variables

---
+++ variables.dart
@@ -0,0 +1,27 @@
+void main(){
+    //variables
+
+    // Declaring but not assigned
+    var myName;
+
+    //String
+    //var name = "Destiny Saturday";
+    String name = "Destiny Saturday";
+
+    //int
+    // var age = 20;
+    int age = 20;
+
+    //Dynamic
+    dynamic firstName = "Destiny";
+
+    //Const
+    const String fullName = "Destiny Saturday";
+    final String nickName = "Dee";
+
+    print(myName);
+
+    myName = "Destiny";
+    print(myName);
+
+}
